6eaa4346a4ef0460f6743534037c6d59aa23122e
Ibuki Nakamura <Ibuki>
2020-07-22 00:35:49 +0900

    Fix sql scripts

--- data_seed_scripts/2_json2sql.py
+++ data_seed_scripts/2_json2sql.py
@@ -20,19 +20,22 @@ for t in df:
         except Exception as err:
             type_str = "未分類"
         emoji_pattern = re.compile("["
-            u"\U0001F600-\U0001F64F"
-            u"\U0001F300-\U0001F5FF"
-            u"\U0001F680-\U0001F6FF"
-            u"\U0001F1E0-\U0001F1FF"
-                            "]+", flags=re.UNICODE)
+                                   u"\U0001F600-\U0001F64F"
+                                   u"\U0001F300-\U0001F5FF"
+                                   u"\U0001F680-\U0001F6FF"
+                                   u"\U0001F1E0-\U0001F1FF"
+                                   "]+", flags=re.UNICODE)
+
         def remove_emoji(src_str):
             return ''.join(c for c in src_str if c not in emoji.UNICODE_EMOJI)
-        _dt = ("{:0>2}-{:0>2}-{:0>2} {:0>2}:{:0>2}:{:0>2}").format(dt_p[0],dt_p[1],dt_p[2],dt_p[3],dt_p[4],dt_p[5])
+        _dt = ("{:0>2}-{:0>2}-{:0>2} {:0>2}:{:0>2}:{:0>2}").format(
+            dt_p[0], dt_p[1], dt_p[2], dt_p[3], dt_p[4], dt_p[5])
         sql += "INSERT INTO articles (id,title,detail,type,img_url,created_at,updated_at) VALUES ({},\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\");\n".format(
             str(count),
-            t["title"].replace("'",""),
-            remove_emoji(emoji_pattern.sub(r'@emoji', t["summary"].replace("'","").replace("\u3000","").replace("\"","\\\"").replace("\n",""))),
-            type_str.replace("'",""),
+            t["title"].replace("'", ""),
+            remove_emoji(emoji_pattern.sub(r'@emoji', t["summary"].replace(
+                "'", "").replace("\u3000", "").replace("\"", "\\\"").replace("\n", ""))),
+            type_str.replace("'", ""),
             img_url,
             _dt,
             _dt
@@ -41,5 +44,5 @@ for t in df:
     except Exception as e:
         print(e)
 
-with open('2_articles.sql', 'w') as f:
+with open('3_articles.sql', 'w') as f:
     f.write(sql)
