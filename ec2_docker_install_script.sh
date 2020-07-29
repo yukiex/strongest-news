cc5be96cf8da832db02e22cb0987db1e6340a5e4
Ibuki Nakamura <23179726+inakam@users.noreply.github.com>
2020-07-29 11:09:24 +0900

    Create ec2_docker_install_script.sh

---
+++ ec2_docker_install_script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
